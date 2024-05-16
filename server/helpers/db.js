6f3ebb0cb9346f1e9ef98d8c15d4be9989537022
Tabishdawood45 <Tabishdawood54@gmail.com>
2024-05-16 19:04:05 +0300

    ssl included

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -21,10 +21,9 @@ const openDb = () => {
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
+        ssl: process.env.SSL
     })
     return pool
 }
 
-module.exports = { 
-    query 
-}
+module.exports = { query }
